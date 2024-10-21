b29eca7f992dcfd7acd8e78a6fa48fff361d9fb7
Amit Kumar Singh <157602171+Amit08062001@users.noreply.github.com>
2024-10-21 09:41:46 +0000

    Deploy project1

--- script.js
+++ script.js
@@ -1,46 +1,32 @@
-// Array to hold the image sources and descriptions
-var images = [
-    { src: 'image1.avif', description: 'This is the first image description' },
-    { src: 'image2.avif', description: 'This is the second image description' },
-    { src: 'image3.avif', description: 'This is the third image description' }
-    // Add more images as needed
-  ];
-  
-  var currentIndex = 0;
-  
-  // Function to open the modal and display the image with description
-  function openModal(index) {
-    currentIndex = index;
-    var modal = document.getElementById("myModal");
-    var modalImg = document.getElementById("modalImage");
-    var captionText = document.getElementById("modalCaption");
-  
-    modal.style.display = "block"; // Display modal
-    modalImg.src = images[currentIndex].src; // Set image source
-    captionText.innerHTML = images[currentIndex].description; // Set description
-  }
-  
-  // Function to close the modal
-  function closeModal() {
-    var modal = document.getElementById("myModal");
-    modal.style.display = "none"; // Hide modal
-  }
-  
-  // Function to change the image when next/previous buttons are clicked
-  function changeImage(direction) {
-    currentIndex += direction;
-  
-    // If currentIndex exceeds the range, wrap around
-    if (currentIndex >= images.length) {
-      currentIndex = 0; // Loop back to the first image
-    } else if (currentIndex < 0) {
-      currentIndex = images.length - 1; // Go to the last image
-    }
-  
-    var modalImg = document.getElementById("modalImage");
-    var captionText = document.getElementById("modalCaption");
-  
-    modalImg.src = images[currentIndex].src; // Update image source
-    captionText.innerHTML = images[currentIndex].description; // Update description
-  }
-  
+(function() {
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(e) {
+            let value = e.target.dataset.num;
+            if (value !== undefined) {
+                screen.value += value;
+            }
+        });
+    });
+
+    equal.addEventListener('click', function(e) {
+        if (screen.value === '') {
+            screen.value = "Please enter";
+        } else {
+            try {
+                let answer = eval(screen.value);
+                screen.value = answer;
+            } catch (error) {
+                screen.value = "Error";
+            }
+        }
+    });
+
+    clear.addEventListener('click', function(e) {
+        screen.value = "";
+    });
+})()
